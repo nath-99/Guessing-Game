d9d622c425ad6e0df6bd06cdc019ca0e24546d6f
nath-99 <63717462+nath-99@users.noreply.github.com>
2021-07-07 23:52:45 +0530

    Update Brain.js

--- Brain.js
+++ Brain.js
@@ -10,7 +10,7 @@ var number = Math.floor(Math.random() * 1000)+1;
 
 var audio = document.getElementById('audio');
 var playPauseBTN = document.getElementById('playBTN');
-var count = 0;
+var attempts=12;
 
 /*
 function playm()
@@ -29,9 +29,11 @@ function playm()
 	}
 }
 */
+document.getElementById("attempt").textContent="You Have total : "+attempts.toString();
 
 function play()
-{noofguess+=1;
+{   
+	noofguess+=1;
 
     var user_input = document.getElementById("input").value;
 
@@ -41,9 +43,17 @@ function play()
 
 	else 
 	{
+    attempts-=1;
+
+	if(attempts<=2){document.getElementById("attempt").style.color = "red";}
+
+	document.getElementById("attempt").textContent=" Now You Have total : "+attempts.toString()+" left";
+
 	guessed_nums.push(user_input);
 
-	if(Math.abs(user_input-number)<=5 && Math.abs(user_input-number)>0)output.textContent="You are very close "+String.fromCodePoint(0x1F600);
+	if(user_input!=number && attempts==0){ attempt(); return; }
+
+	else if(Math.abs(user_input-number)<=5 && Math.abs(user_input-number)>0)output.textContent="You are very close "+String.fromCodePoint(0x1F600);
 	
 	else if(user_input>number && Math.abs(user_input-number)>5)output.textContent="Enter a lower number plzz "+String.fromCodePoint(0x1F615);
 	
@@ -60,4 +70,10 @@ function mseg(noofguess)
     if(noofguess<10)msg.textContent="You guessed it Right after "+ noofguess +" attempts "+String.fromCodePoint(0x1F929)+" Good job";
 
 	else msg.textContent="You guessed it after "+ noofguess +" attempts "+String.fromCodePoint(0x1F642)+" Do better guesses nxt time";
+}
+
+function attempt()
+{
+	s.style.display = "none";
+    document.getElementById("lost").textContent="You have exhausted all ur attempts : "+String.fromCodePoint(0x1F641)+"\n"+"Best Of Luck for nxt turn"+String.fromCodePoint(0x1F44D);
 }
